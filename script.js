840dcca6a8e52027707e70704415261bb0afaf75
Batu Özdemir <batuozdemir@Batus-MacBook-Air.local>
2024-03-11 18:18:57 +0300

    Simple version2

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const cardImage = document.getElementById('cardImage');
 const cardText = document.getElementById('cardText');
 const cardCommentary = document.getElementById('cardCommentary');
 
-const totalCards = 10;
+const totalCards = 79;
 
 const cardData = {
   "1": {
@@ -325,13 +325,18 @@ const cardData = {
   }
 };
 
-drawButton.addEventListener('click', function() {
+// Function to draw a random card
+function drawRandomCard() {
   const randomCard = Math.floor(Math.random() * totalCards) + 1;
   const imagePath = `CardPictures/card_${randomCard}.jpg`;
-
   cardImage.src = imagePath;
   cardText.textContent = cardData[randomCard].text;
   cardCommentary.textContent = cardData[randomCard].commentary;
-
   cardContainer.style.display = 'block';
-});
+}
+
+// Draw a random card when the page loads
+window.addEventListener('load', drawRandomCard);
+
+// Draw a random card when the button is clicked
+drawButton.addEventListener('click', drawRandomCard);
